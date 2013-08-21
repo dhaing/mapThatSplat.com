872eb98c5f7dce444dba1f81959f568c889ddbfd
Jez Higgins <jez@jezuk.co.uk>
2013-08-21 19:07:31 +0100

    Load splays

--- leaflet/leaflet_embed.js
+++ leaflet/leaflet_embed.js
@@ -1,18 +1,63 @@
 var map;
 var ajaxRequest;
-var plotlist;
-var plotlayers=[];
+var splatlist;
+var splatlayers=[];
 
 function initmap() {
-	// set up the map
-	map = new L.Map('map');
+    // set up the map
+    map = new L.Map('map');
 
-	// create the tile layer with correct attribution
-	var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-	var osmAttrib='Map data © OpenStreetMap contributors';
-	var osm = new L.TileLayer(osmUrl, {minZoom: 8, maxZoom: 12, attribution: osmAttrib});		
+    // create the tile layer with correct attribution
+    var osmUrl='http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    var osmAttrib='Map data © OpenStreetMap contributors';
+    var osm = new L.TileLayer(osmUrl, {minZoom: 6, maxZoom: 14, attribution: osmAttrib});
 
-	// start the map in South-East England
-	map.setView(new L.LatLng(51.3, 0.7),9);
-	map.addLayer(osm);
+    map.setView(new L.LatLng(52.5, -1.8),9);
+    map.addLayer(osm);
+
+    ajaxRequest = getXmlHttpObject();
+    askForSplats();
+}
+
+function getXmlHttpObject() {
+    if (window.XMLHttpRequest) { return new XMLHttpRequest(); }
+    if (window.ActiveXObject)  { return new ActiveXObject("Microsoft.XMLHTTP"); }
+    return null;
+}
+
+function askForSplats() {
+    var msg = "/mapThatSplat/splats.php";
+    ajaxRequest.onreadystatechange = splatsDownloaded;
+    ajaxRequest.open('GET', msg, true);
+    ajaxRequest.send(null);
+}
+
+function splatsDownloaded() {
+    // if AJAX returned a list of markers, add them to the map
+    if (ajaxRequest.readyState==4) {
+	//use the info here that was returned
+	if (ajaxRequest.status==200) {
+	    splatlist=eval("(" + ajaxRequest.responseText + ")");
+	    splatTheSplats(splatlist);
+	}
+    }
+}
+
+function splatTheSplats(splatlist) {
+    removeSplats();
+    for (i=0;i<splatlist.length;i++) {
+	var splatll = new L.LatLng(splatlist[i].lat,splatlist[i].lon, true);
+	var splatmark = new L.Marker(splatll);
+	splatmark.data=splatlist[i];
+	map.addLayer(splatmark);
+	splatmark.bindPopup("<h5>"+splatlist[i].name+"</h5><img style='width: 100px' src='"+splatlist[i].img+"'>");
+	splatlayers.push(splatmark);
+    }
+}
+
+function removeSplats() {
+    for (i=0;i<splatlayers.length;i++) {
+	map.removeLayer(splatlayers[i]);
+    }
+    splatlayers=[];
 }
